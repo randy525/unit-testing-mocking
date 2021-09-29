78496aad2c296a0ebfc6c427c776ac60b1f702dc
Ilia Simlih <ilia.simlih@endava.com>
2021-09-29 12:52:46 +0300

    completed tasks

---
+++ src/test/java/com/endava/internship/mocking/repository/InMemPaymentRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.endava.internship.mocking.repository;
+
+import com.endava.internship.mocking.model.Status;
+import com.endava.internship.mocking.model.User;
+import com.endava.internship.mocking.repository.InMemUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InMemPaymentRepositoryTest {
+
+    InMemUserRepository userRepository;
+
+    @BeforeEach
+    void init() {
+        userRepository = new InMemUserRepository();
+    }
+
+    @Test
+    void findById() {
+        User expectedUser = new User(1, "John", Status.ACTIVE);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> userRepository.findById(null));
+        assertEquals(exception.getMessage(), "User id must not be null");
+
+        assertEquals(userRepository.findById(0), Optional.empty());
+        assertEquals(userRepository.findById(1), Optional.of(expectedUser));
+    }
+}
