75d83b5dd238b2f59a76c01f03efb654e198d9d3
Ilia Simlih <ilia.simlih@endava.com>
2021-09-30 09:20:32 +0300

    added new asserts and tests in unhappy cases

--- src/test/java/com/endava/internship/mocking/service/BasicValidationServiceTest.java
+++ src/test/java/com/endava/internship/mocking/service/BasicValidationServiceTest.java
@@ -5,6 +5,8 @@ import com.endava.internship.mocking.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BasicValidationServiceTest {
@@ -15,36 +17,65 @@ public class BasicValidationServiceTest {
     void init() {
         validationService = new BasicValidationService();
     }
+
     @Test
-    public void validateAmount() {
+    public void validateAmount_HappyCase() {
+        assertDoesNotThrow(() -> validationService.validateAmount(100.0));
+    }
+
+    @Test
+    public void validateAmountWhenPutNull() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> validationService.validateAmount(null));
         assertEquals(exception.getMessage(), "Amount must not be null");
-        exception = assertThrows(IllegalArgumentException.class, () -> validationService.validateAmount(-5.7));
+    }
+
+    @Test
+    public void validateAmountWhenPutNegativeNumber() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> validationService.validateAmount(-5.7));
         assertEquals(exception.getMessage(), "Amount must be greater than 0");
     }
 
     @Test
-    public void validatePaymentId() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> validationService.validatePaymentId(null));
-        assertEquals(exception.getMessage(), "Payment id must not be null");
-        //System.out.println("Test print");
+    public void validatePaymentId_HappyCase() {
+        assertDoesNotThrow(() -> validationService.validatePaymentId(UUID.randomUUID()));
     }
 
     @Test
-    public void validateUserId() {
+    public void validatePaymentIdWithNullArgument() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> validationService.validatePaymentId(null));
+        assertEquals(exception.getMessage(), "Payment id must not be null");
+    }
+
+    @Test
+    public void validateUserId_HappyCase() {
+        assertDoesNotThrow(() -> validationService.validateUserId(5));
+    }
+
+    @Test
+    public void validateUserIdWithNullArgument() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> validationService.validateUserId(null));
         assertEquals(exception.getMessage(), "User id must not be null");
     }
 
     @Test
-    public void validateUser() {
+    public void validateUser_HappyCase() {
+        assertDoesNotThrow(() -> validationService.validateUser(new User(1, "Name", Status.ACTIVE)));
+    }
+
+    @Test
+    public void validateUserInactive() {
         User user = new User(10, "John", Status.INACTIVE);
         Exception exception = assertThrows(IllegalArgumentException.class, () -> validationService.validateUser(user));
         assertEquals(exception.getMessage(), "User with id " + user.getId() + " not in ACTIVE status");
     }
 
     @Test
-    public void validateMessage() {
+    public void validateMessage_HappyCase() {
+        assertDoesNotThrow(() -> validationService.validateMessage("Some message"));
+    }
+
+    @Test
+    public void validateMessageWithNullArgument() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> validationService.validateMessage(null));
         assertEquals(exception.getMessage(), "Payment message must not be null");
     }
--- src/test/java/com/endava/internship/mocking/service/PaymentServiceTest.java
+++ src/test/java/com/endava/internship/mocking/service/PaymentServiceTest.java
@@ -3,6 +3,7 @@ package com.endava.internship.mocking.service;
 import com.endava.internship.mocking.model.Payment;
 import com.endava.internship.mocking.model.Status;
 import com.endava.internship.mocking.model.User;
+import com.endava.internship.mocking.repository.InMemPaymentRepository;
 import com.endava.internship.mocking.repository.PaymentRepository;
 import com.endava.internship.mocking.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +13,8 @@ import org.mockito.ArgumentCaptor;
 import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.Optional;
-import java.util.UUID;
+
+import java.util.*;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentServiceTest {
@@ -33,7 +34,7 @@ class PaymentServiceTest {
     }
 
     @Test
-    void createPayment() {
+    void createPaymentInteractionWithCollaboratorsCheck() {
         User expectedUser = new User(0, "John", Status.ACTIVE);
 
         when(userRepositoryMock.findById(anyInt())).thenReturn(Optional.of(expectedUser));
@@ -52,24 +53,56 @@ class PaymentServiceTest {
         assertEquals(argumentCaptor.getValue().getUserId(), expectedUser.getId());
         assertEquals(argumentCaptor.getValue().getAmount(), 150.5);
         assertTrue(argumentCaptor.getValue().getMessage().contains(expectedUser.getName()));
+
+        verifyNoMoreInteractions(validationServiceMock);
+        verifyNoMoreInteractions(paymentRepositoryMock);
+        verifyNoMoreInteractions(userRepositoryMock);
     }
 
     @Test
-    void editMessage() {
-        UUID paymentID = UUID.randomUUID();
-        String newString = "Any String";
+    void createPaymentSearchingForNonExistentUser() {
+        assertThrows(NoSuchElementException.class, () -> paymentService.createPayment(0, 150.5));
+    }
+
+    @Test
+    void editMessageInteractionWithCollaboratorsCheck() {
+        final UUID paymentID = UUID.randomUUID();
+        final String newString = "Any String";
         paymentService.editPaymentMessage(paymentID, newString);
 
         verify(validationServiceMock).validatePaymentId(paymentID);
         verify(validationServiceMock).validateMessage(newString);
 
         verify(paymentRepositoryMock).editMessage(paymentID, newString);
+
+        verifyNoMoreInteractions(validationServiceMock);
+        verifyNoMoreInteractions(paymentRepositoryMock);
     }
 
     @Test
     void getAllByAmountExceeding() {
         final double amount = 13.37;
-        paymentService.getAllByAmountExceeding(amount);
+
+        List<Payment> paymentsList = Arrays.asList(
+                new Payment(1, 15.0, "text"),
+                new Payment(2, 5.0, "text"),
+                new Payment(3, 10.0, "text"),
+                new Payment(4, 17.0, "text"),
+                new Payment(5, 14.0, "text")
+        );
+
+        List<Payment> expectedList = Arrays.asList(
+                paymentsList.get(0),
+                paymentsList.get(3),
+                paymentsList.get(4)
+        );
+
+        when(paymentRepositoryMock.findAll()).thenReturn(paymentsList);
+
+        assertEquals(paymentService.getAllByAmountExceeding(amount), expectedList);
+
         verify(paymentRepositoryMock).findAll();
+
+        verifyNoMoreInteractions(paymentRepositoryMock);
     }
 }
